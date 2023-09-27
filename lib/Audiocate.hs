540c350661d5d762196f6a52c2bdc5cf024ddb96
sigrdrifa <corynz@pm.me>
2023-09-27 17:12:57 +0100

    add TOTP calculate and check

--- lib/Audiocate.hs
+++ lib/Audiocate.hs
@@ -1,8 +1,15 @@
 module Audiocate (someFunc) where
 
-import Data.Audio.Wave()
-import Stego.Common()
+import Data.Audio.Wave ()
+import Data.Text qualified as T
+import Data.Text.Encoding (encodeUtf8)
+import Data.Time.Clock (getCurrentTime)
+import Stego.Common (EncodingType (LsbEncoding), StegoParams (StegoParams), calculateTotp, checkTotp)
 
 someFunc :: IO ()
 someFunc = do
-  putStrLn "Hello, haskell"
+  time <- getCurrentTime
+  let stegoParams = StegoParams (encodeUtf8 (T.pack "my secret")) 30 6 LsbEncoding 123
+  let totpValue = calculateTotp stegoParams time
+  print totpValue
+  print $ checkTotp stegoParams time totpValue
