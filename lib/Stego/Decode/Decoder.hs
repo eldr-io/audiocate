e46c0ee1d679fc79d15e98dab5dacea8850d1e50
sigrdrifa <corynz@pm.me>
2023-09-29 00:44:03 +0100

    STM structures

--- lib/Stego/Decode/Decoder.hs
+++ lib/Stego/Decode/Decoder.hs
@@ -2,16 +2,179 @@
 - Decoding processor which allows for the enqueueing of Frames
 - to be decoded and runs a dedicated thread for dequeueing and decoding frames
 -}
-module Stego.Decode.Decoder ( Decoder (..)) where
+module Stego.Decode.Decoder (
+  newDecoder,
+  getResultChannel,
+  enqueueFrame,
+  stopDecoder,
+  DecoderResult (..),
+  mapDecoderOpQToResultChan,
+  DecoderResultList,
+  getResultStats,
+)
+where
 
-import Control.Concurrent.STM (TMVar, TQueue)
+import Control.Concurrent (forkIO)
+import Control.Concurrent.STM (
+  TChan,
+  TMVar,
+  TQueue,
+  dupTChan,
+  newBroadcastTChanIO,
+  newEmptyTMVarIO,
+  newTQueueIO,
+  putTMVar,
+  readTChan,
+  readTQueue,
+  writeTChan,
+  writeTQueue,
+ )
+import Control.Monad (void)
+import Control.Monad.STM (atomically)
 import Data.Audio.Wave (Frame)
-import Stego.Common (DecodedFrame, StegoParams (..))
+import Data.List (foldl')
+import Stego.Common (DecodedFrame, EncodingType (..), StegoParams (..), checkTotp, getEncodingType, readBinWord32, readBinWord64, word64ToUtcTime)
+import Stego.Decode.LSB qualified
+import Stego.Encode.Encoder qualified as EC
 
-type FrameQ = TQueue Frame
+data DecoderOp = DecodeFrame Frame | StopDecoder (TMVar ())
+
+type DecoderOpQ = TQueue DecoderOp
+
+data DecoderResult = DecodedFrameR DecodedFrame Bool | SkippedFrame Frame | StoppingDecoder
+
+type DecoderResultList = [DecoderResult]
+
+data DecoderResultStats = DRS {totalFrames, verifiedFrames, failedFrames, skippedFrames :: !Int}
+
+instance Show DecoderResultStats where
+  show d =
+    "=========================\n   Total Frames : "
+      ++ show (totalFrames d)
+      ++ "\n   Skipped      : "
+      ++ show (skippedFrames d)
+      ++ "\n   Unverified   : "
+      ++ show (failedFrames d)
+      ++ "\n   Verified     : "
+      ++ show (verifiedFrames d)
+      ++ " ("
+      ++ show (round (verifiedScore * 100.0))
+      ++ "%)"
+      ++ "\n=========================\n"
+   where
+    verifiedScore = if verifiedFrames d == 0 then 0.0 else fromIntegral (verifiedFrames d) / fromIntegral (totalFrames d - skippedFrames d)
 
 data Decoder = Decoder
   { stegoParams :: StegoParams
-  , frameQ :: FrameQ
-  , finishedMutex :: TMVar [DecodedFrame]
+  , opQ :: DecoderOpQ
+  , resultChan :: TChan DecoderResult
   }
+
+getResultStats :: DecoderResultList -> DecoderResultStats
+getResultStats dcl = countStatsDcl' dcl (DRS 0 0 0 0)
+
+countStatsDcl' :: DecoderResultList -> DecoderResultStats -> DecoderResultStats
+countStatsDcl' = flip (foldl' go)
+ where
+  go (DRS p q r s) f = case f of
+    DecodedFrameR _ True ->
+      DRS (p + 1) (q + 1) r s
+    DecodedFrameR _ False ->
+      DRS (p + 1) q (r + 1) s
+    SkippedFrame _ ->
+      DRS (p + 1) q r (s + 1)
+    _ ->
+      DRS (p + 1) q r s
+
+{- | Used to traverse a DecoderResultList only once and accumulate all interesting
+count statistics, returning a tuple of
+(totalFrames, totalVerified, totalFailed, totalSkipped)
+-}
+countStatsDcl :: DecoderResultList -> (Int, Int, Int, Int) -> DecoderResultStats
+countStatsDcl [] (p, q, r, s) = DRS p q r s
+countStatsDcl ((DecodedFrameR _ True) : xs) (!p, !q, !r, !s) = countStatsDcl xs (p + 1, q + 1, r, s)
+countStatsDcl ((DecodedFrameR _ False) : xs) (!p, !q, !r, !s) = countStatsDcl xs (p + 1, q, r + 1, s)
+countStatsDcl ((SkippedFrame _) : xs) (!p, !q, !r, !s) = countStatsDcl xs (p + 1, q, r, s + 1)
+countStatsDcl (_ : xs) (!p, !q, !r, !s) = countStatsDcl xs (p + 1, q, r, s)
+
+{- | Creates a new decoder that can decode frames using
+the provided StegoParams
+-}
+newDecoder :: StegoParams -> IO Decoder
+newDecoder stegoParams = do
+  opQ <- newTQueueIO
+  resultChan <- newBroadcastTChanIO
+  let dec = Decoder stegoParams opQ resultChan
+  void $ forkIO $ runDecoder dec
+  return dec
+
+getResultChannel :: Decoder -> IO (TChan DecoderResult)
+getResultChannel = atomically . dupTChan . resultChan
+
+enqueueFrame :: Decoder -> Frame -> IO ()
+enqueueFrame dec f = atomically $ writeTQueue (opQ dec) (DecodeFrame f)
+
+{- | Sets the stop signal TMVar for the decoder to break out
+it's run loop.
+-}
+stopDecoder :: Decoder -> IO (TMVar ())
+stopDecoder dec = do
+  m <- newEmptyTMVarIO
+  atomically $ writeTQueue (opQ dec) (StopDecoder m)
+  pure m
+
+{- | Creates a mapping thread that forwards Ops coming from an
+EncoderResult channel into Decoder Ops. Used for sequencing so that
+encoder results can immedeatily be handled by a decoder.
+-}
+mapDecoderOpQToResultChan :: Decoder -> TChan EC.EncoderResult -> IO ()
+mapDecoderOpQToResultChan dec channel = void $ forkIO loop
+ where
+  loop = do
+    res <- atomically $ readTChan channel
+    case res of
+      (EC.EncodedFrame f) -> do
+        enqueueFrame dec f
+        loop
+      EC.StoppingEncoder -> do
+        _ <- stopDecoder dec
+        putStrLn "Received stopping encoder signal on chan"
+      (EC.SkippedFrame f) -> do
+        enqueueFrame dec f
+        loop
+
+decodeFrame' :: StegoParams -> Frame -> Maybe DecodedFrame
+decodeFrame' stegoParams frame
+  | length frame < 128 = Nothing
+  | getEncodingType stegoParams == LsbEncoding =
+      Just (frame, (time64, totp32))
+  | otherwise = Nothing
+ where
+  (time, totp) = Stego.Decode.LSB.decodeFrame frame
+  time64 = readBinWord64 time
+  totp32 = readBinWord32 totp
+
+runDecoder :: Decoder -> IO ()
+runDecoder dec = loop
+ where
+  toResult frame Nothing = SkippedFrame frame
+  toResult _ (Just (frame, (time, payload))) =
+    DecodedFrameR
+      (frame, (time, payload))
+      (checkTotp (stegoParams dec) (word64ToUtcTime time) payload)
+  loop = do
+    op <- atomically $ readTQueue (opQ dec)
+    case op of
+      (DecodeFrame f) -> do
+        let shouldSkip = realToFrac (sum (map abs (take 128 f))) < (1E-3 :: Double)
+        if shouldSkip
+          then do
+            atomically $ writeTChan (resultChan dec) (SkippedFrame f)
+            loop
+          else do
+            let decoded = decodeFrame' (stegoParams dec) f
+            atomically $ writeTChan (resultChan dec) (toResult f decoded)
+            loop
+      (StopDecoder m) -> atomically $ do
+        writeTChan (resultChan dec) StoppingDecoder
+        putTMVar m ()
--- lib/Stego/Encode/Encoder.hs
+++ lib/Stego/Encode/Encoder.hs
@@ -1,82 +1,105 @@
-{- |
-- Encoding processor which allows for the enqueueing of Frames
-- to be encoded and runs a dedicated thread for dequeueing and encoding frames
--}
-module Stego.Encode.Encoder (
-  newEncoder,
-  enqueueFrame,
-  startEncoder,
-  takeStopVar,
-)
+-- |
+-- - Encoding processor which allows for the enqueueing of Frames
+-- - to be encoded and runs a dedicated thread for dequeueing and encoding frames
+module Stego.Encode.Encoder
+  ( newEncoder,
+    enqueueFrame,
+    stopEncoder,
+    getResultChannel,
+    EncoderResult (..),
+    EncoderResultChan,
+  )
 where
 
 import Control.Concurrent (forkIO, threadDelay)
-import Control.Concurrent.STM (TMVar, TQueue, isEmptyTQueue, newEmptyTMVarIO, newTQueueIO, putTMVar, readTQueue, takeTMVar, writeTQueue)
+import Control.Concurrent.STM
+  ( TChan,
+    TMVar,
+    TQueue,
+    dupTChan,
+    newBroadcastTChanIO,
+    newEmptyTMVarIO,
+    newTQueueIO,
+    putTMVar,
+    readTQueue,
+    writeTChan,
+    writeTQueue,
+  )
 import Control.Monad (void)
 import Control.Monad.STM (atomically)
-import Data.Audio.Wave (Frame, Frames)
+import Data.Audio.Wave (Frame)
 import Data.Time (UTCTime, getCurrentTime)
 import Stego.Common (EncodingType (LsbEncoding), StegoParams (..), calculateTotp, getEncodingType, utcTimeToWord64)
 import Stego.Encode.LSB (encodeFrame)
 
-type FrameQ = TQueue Frame
+data EncoderOp = EncodeFrame Frame | StopEncoder (TMVar ())
+
+data EncoderResult = EncodedFrame Frame | SkippedFrame Frame | StoppingEncoder
+
+type EncoderOpQ = TQueue EncoderOp
+
+type EncoderResultChan = TChan EncoderResult
 
 data Encoder = Encoder
-  { stegoParams :: StegoParams
-  , frameQ :: FrameQ
-  , finishedMutex :: TMVar Frames
+  { stegoParams :: StegoParams,
+    encoderOpQ :: EncoderOpQ,
+    resultChan :: EncoderResultChan
   }
 
--- | Creates a new frame queue
-newFrameQueue :: IO FrameQ
-newFrameQueue = newTQueueIO
-
-newFinishedMutex :: IO (TMVar Frames)
-newFinishedMutex = newEmptyTMVarIO
-
+-- | Creates a new encoder that can encode frames using
+-- the provided StegoParams
 newEncoder :: StegoParams -> IO Encoder
 newEncoder stegoParams = do
-  frameQ <- newFrameQueue
-  finishedMutex <- newFinishedMutex
-  let enc = Encoder stegoParams frameQ finishedMutex
+  frameQ <- newTQueueIO
+  encFrameChan <- newBroadcastTChanIO
+  let enc = Encoder stegoParams frameQ encFrameChan
+  void $ forkIO $ runEncoder enc
   return enc
 
-stopEncoder :: Encoder -> Frames -> IO ()
-stopEncoder enc frames = atomically $ putTMVar (finishedMutex enc) frames
+-- | Returns a duplicate of the Broadcast result channel used 
+-- by the encoder.
+getResultChannel :: Encoder -> IO (TChan EncoderResult)
+getResultChannel = atomically . dupTChan . resultChan
 
-takeStopVar :: Encoder -> IO Frames
-takeStopVar enc = atomically $ takeTMVar (finishedMutex enc)
+-- | Sets the stop signal TMVar for the encoder to break out
+-- it's encoding loop.
+stopEncoder :: Encoder -> IO (TMVar ())
+stopEncoder enc = do
+  m <- newEmptyTMVarIO
+  atomically $ writeTQueue (encoderOpQ enc) (StopEncoder m)
+  pure m
 
-dequeueFrame :: Encoder -> IO Frame
-dequeueFrame enc = do
-  frame <- atomically $ readTQueue (frameQ enc)
-  putStrLn $ "Dequeueing frame:" <> show (length frame)
-  threadDelay 500
-  return frame
-
+-- | Enqueues a frame for the encoder to encode
 enqueueFrame :: Encoder -> Frame -> IO ()
-enqueueFrame enc frame = do
-  atomically $ writeTQueue (frameQ enc) frame
+enqueueFrame enc frame = atomically $ writeTQueue (encoderOpQ enc) (EncodeFrame frame)
 
+-- | Used internally by the encoder to call on the appropriate encoding
+-- technique as dictated by the stegoParams
 encodeFrame' :: StegoParams -> UTCTime -> Frame -> Frame
 encodeFrame' stegoParams time frame
   | getEncodingType stegoParams == LsbEncoding =
       Stego.Encode.LSB.encodeFrame time64 totp frame
   | otherwise = undefined
- where
-  time64 = utcTimeToWord64 time
-  totp = calculateTotp stegoParams time
+  where
+    time64 = utcTimeToWord64 time
+    totp = calculateTotp stegoParams time
 
-startEncoder :: Encoder -> IO ()
-startEncoder encoder = do
-  void $ forkIO $ proc encoder []
- where
-  proc :: Encoder -> Frames -> IO ()
-  proc enc frames = do
-    frame <- dequeueFrame enc
-    time <- getCurrentTime
-    let encodedFrame = encodeFrame' (stegoParams enc) time frame
-    let frs = frames ++ [encodedFrame]
-    isEmpty <- atomically $ isEmptyTQueue (frameQ enc)
-    putStrLn $ "Checking if is empty: " <> show isEmpty
-    if isEmpty then stopEncoder enc frs else proc enc frs
+runEncoder :: Encoder -> IO ()
+runEncoder enc = loop
+  where
+    loop = do
+      op <- atomically $ readTQueue (encoderOpQ enc)
+      case op of
+        (EncodeFrame f) -> do
+          time <- getCurrentTime
+          let shouldSkip = realToFrac (sum (map abs (take 128 f))) < (1E-3 :: Double)
+          if shouldSkip then do
+            atomically $ writeTChan (resultChan enc) (SkippedFrame (replicate (length f) 0))
+            loop
+          else do
+            let encoded = encodeFrame' (stegoParams enc) time f
+            atomically $ writeTChan (resultChan enc) (EncodedFrame encoded)
+            loop
+        (StopEncoder m) -> atomically $ do
+          writeTChan (resultChan enc) StoppingEncoder
+          putTMVar m ()
