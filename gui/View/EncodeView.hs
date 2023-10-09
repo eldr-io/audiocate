8a84ae0245c6b4ab1142dbccd6c88998419ec1ae
sigrdrifa <corynz@pm.me>
2023-10-09 18:13:15 +0100

    basic encodeview plumbing to loadFile

--- gui/View/EncodeView.hs
+++ gui/View/EncodeView.hs
@@ -1,24 +1,48 @@
 module View.EncodeView (
   EncodeView (..),
   initEncodeView,
+  updateEncodeViewAudioFileLoaded,
 )
 where
 
+import AppState (AppState (loadedAudio), AppStateLoadedAudio (loadedAudioWave))
 import Data.GI.Base
 import Data.Maybe (fromJust)
 import Data.Text qualified as T
 import GI.Gtk qualified as Gtk
-import qualified GI.Adw as Adw
+import Control.Concurrent (readMVar)
+import Data.Audio.Wave (WaveAudio(..))
 
 data EncodeView = EncodeView
   { title :: T.Text
   , id :: T.Text
-  , encodeViewBox :: Gtk.Box
+  , appState :: AppState
+  , encodeViewBox :: !Gtk.Box
+  , encFilePropSrcLbl :: !Gtk.Label
+  -- , encFilePropRateLbl :: !Gtk.Label
+  -- , encFilePropBitRateLbl :: !Gtk.Label
+  -- , encFilePropSamplesLbl :: !Gtk.Label
+  -- , encFilePropNumFramesLbl :: !Gtk.Label
+  -- , encFilePropNumChannelsLbl :: !Gtk.Label
   }
 
-initEncodeView :: IO EncodeView
-initEncodeView = do
+updateEncodeViewAudioFileLoaded :: AppState -> EncodeView -> IO ()
+updateEncodeViewAudioFileLoaded appState encodeView = do
+  putStrLn "update encodeview called"
+  audio <- readMVar $ loadedAudio appState
+  let wa = loadedAudioWave audio
+  let srcLbl = encFilePropSrcLbl encodeView
+  Gtk.labelSetLabel srcLbl (T.pack (srcFile wa))
+
+
+initEncodeView :: AppState -> IO EncodeView
+initEncodeView appState = do
   builder <- Gtk.builderNewFromResource "/gui/View/EncodeView.ui"
   encodeBin <- Gtk.builderGetObject builder "encTopBox"
   bin <- castTo Gtk.Box (fromJust encodeBin)
-  pure $ EncodeView "Encode" "encodeView" (fromJust bin)
+
+  encFilePropSrcLblPtr <- fromJust <$> Gtk.builderGetObject builder "encFilePropSrcLbl"
+  encFilePropSrcLbl <- fromJust <$> castTo Gtk.Label encFilePropSrcLblPtr
+  pure
+    $ EncodeView "Encode" "encodeView" appState (fromJust bin)
+    encFilePropSrcLbl
