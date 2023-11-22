ce783ae1c5b9bcc6804105c40ac211ca2652e3d2
sigrdrifa <corynz@pm.me>
2023-11-22 16:45:58 +0000

    more streaming

--- lib/Command/EncodeStreamCmd.hs
+++ lib/Command/EncodeStreamCmd.hs
@@ -10,6 +10,10 @@ import Sound.File.Sndfile (Info(format), getFileInfo)
 import Conduit (mapM_C, runResourceT, mapC, sumC, MonadTrans (lift))
 import Control.Monad (void)
 import Control.Monad.IO.Class (liftIO)
+import Data.Time (getCurrentTime)
+import qualified Data.Vector.Storable as VS
+import Stego.Encode.Encoder (encodeFrame')
+import Data.Audio.Wave (Frame)
 
 -- runEncodeStreamCmd :: StegoParams -> FilePath -> FilePath -> IO ()
 runEncodeStreamCmd stegoP inputFile outputFile = do
@@ -19,4 +23,6 @@ runEncodeStreamCmd stegoP inputFile outputFile = do
   print (CA.frames input)
   print (CA.rate input)
   print (CA.channels input)
-  runConduitRes $ CA.source input .| mapC show .| mapM_C (liftIO . print . length)
+  time <- getCurrentTime
+  runConduitRes $ CA.source input .| mapC VS.toList .| mapC (\x -> [(0,x)] :: [Frame]) .| mapC (map (encodeFrame' stegoP time)) .| mapC (head . map snd) .| mapC VS.fromList .| mapM_C (liftIO . print)
+
