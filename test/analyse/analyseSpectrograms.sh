22da54fb267995fa37c9b653ff270f0d33a47148
sigrdrifa <corynz@pm.me>
2023-12-09 19:47:43 +0000

    update tests

--- test/analyse/analyseSpectrograms.sh
+++ test/analyse/analyseSpectrograms.sh
@@ -3,5 +3,5 @@ for file in *.png
 do
     outfile="${file%.*}_out.png"
     resultFile="${file%.*}_out_compare.png"
-    composite "$file" "$outfile" -compose difference "$resultFile"
+    composite -verbose "$file" "$outfile" -compose difference "$resultFile"
 done
